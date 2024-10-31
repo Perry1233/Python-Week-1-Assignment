851bfcf2d02fc8f23a7719d3f75aebd078e02c1e
Perry1233 <mensahsilas91@gmail.com>
2024-10-31 17:47:41 +0000

    week 1 completed

---
+++ user_input.py
@@ -0,0 +1,24 @@
+#QUESTION 2.
+Name = input("Please enter your name: ")
+
+print(Name)
+
+
+#QUESTION 3.
+Age = input("Please enter your age: ")
+
+print(Age)
+
+
+#QUESTION 4.
+Location = input("Please enter your location: ")
+
+print(Location)
+
+
+#QUESTION 5.
+Name = "Silas Mensah"
+Age = 24
+Location = "Tema, Ghana"
+
+print(f"Hi everyone, my name is {Name}, am {Age} years old and am located at {Location}")
